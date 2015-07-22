fb1168f78e9cb77832c5931d16f175c3557349cf
Priyapanch <priya.panch@gmail.com>
2015-07-22 16:07:44 +0530

    Spec for mutiplication actions

---
+++ CalculatorScripts/Multiply.js
@@ -0,0 +1,21 @@
+describe('super calculator actions' , function(){
+
+    var CalculatorPage = require('./Calc_Page.js');
+    var exparams = browser.params;
+
+    it('MULTIPLICATION',function(){
+        CalculatorPage.navigate();
+        CalculatorPage.EnterFirst(exparams.Mulp.input1);
+        CalculatorPage.SelectOperator('MULTIPLICATION');
+        CalculatorPage.EnterSecond(exparams.Mulp.input2);
+        CalculatorPage.ClickGo();
+        var actualVal = CalculatorPage.GetResult();
+        expect(actualVal).toEqual(exparams.Mulp.output);
+    });
+    it('verify history',function(){
+        var eleHistory = CalculatorPage.GetHistory(0);
+        //expect(eleHistory.count()).toBe(2);
+        expect(eleHistory).toContain(exparams.Mulp.VerifyHistory);
+
+    });
+});
